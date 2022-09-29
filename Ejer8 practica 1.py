5ee0eb524ebdbf46925586cb36886d0d974bb03e
Leo-Cabrera <kanomody29@gmail.com>
2022-09-29 18:38:42 +0200

    d

--- Ejer8 practica 1.py
+++
@@ -1,9 +0,0 @@
-from turtle import *
-pantalla = Screen()
-pantalla.setup(425, 225)
-pantalla.screensize(400, 200)
-tortuga = Turtle()
-r=float(input('¿Cuánto mide el radio?'))
-tortuga.goto(0, -r)
-tortuga.circle(r)
-pantalla.exitonclick()
