b75b3e2731d7a72035d86e922b395f51acda40c3
Leo-Cabrera <kanomody29@gmail.com>
2022-09-29 18:41:47 +0200

    Revert "d"
    This reverts commit 5ee0eb524ebdbf46925586cb36886d0d974bb03e.

---
+++ Ejer9 practica 1.py
@@ -0,0 +1,15 @@
+from turtle import *
+pantalla = Screen()
+pantalla.setup(425, 225)
+pantalla.screensize(400, 200)
+tortuga = Turtle()
+l=float(input('¿Cuánto mide el lado?'))
+r=l/2
+tortuga.goto(0, -r)
+tortuga.circle(r)
+tortuga.goto(r, -r)
+tortuga.goto(r, r)
+tortuga.goto(-r, r)
+tortuga.goto(-r, -r)
+tortuga.goto(0, -r)
+pantalla.exitonclick()
